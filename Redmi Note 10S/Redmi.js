5b8a48d350a2a8ea01bb18dc63de8a88d26eaacd
MEOfortest <153871714+MEOfortest@users.noreply.github.com>
2025-10-15 23:35:53 +0300

    Update Redmi.js

---
+++ Redmi Note 10S/Redmi.js
@@ -0,0 +1,40 @@
+const time_value = document.querySelector("#time")
+const date_value = document.querySelector("#date_details")
+
+function UpdateTime() {
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getHours()
+    let date = time.getDate()
+    let month = time.getMonth()
+    let weekday = time.getDay()
+    let weekdays = [
+        'Вс',
+        'Пн',
+        'Вт',
+        'Ср',
+        'Чт',
+        'Пт',
+        'Сб'
+    ];
+    let weekday_names = weekdays[weekday]
+    let months = [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь',
+    ];
+    let month_names = months[month]
+
+    time_value.innerHTML = `${hours}:${minutes}`;
+    date_value.innerHTML = `${weekday_names}, ${date} ${month_names}`
+}
+setInterval(UpdateTime, 1000)
